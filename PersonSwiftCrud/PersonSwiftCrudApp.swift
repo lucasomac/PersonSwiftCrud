f95600d8e84247f75bb2e20573dfb69ff0ededff
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2024-11-17 12:27:25 -0300

    Initial Commit

---
+++ PersonSwiftCrud/PersonSwiftCrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonSwiftCrudApp.swift
+//  PersonSwiftCrud
+//
+//  Created by Lucas de Oliveira Macedo on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonSwiftCrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
